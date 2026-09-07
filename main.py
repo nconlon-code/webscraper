6189c9a10eb0c54262d52d07b19dae6cb267bb87
Nathan Conlon <nconlon@pm.me>
2026-09-07 10:55:47 -0500

    feat: handle base url cli argument in main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+
 def main():
-    print("Hello from webscraper!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    print(f"starting crawl of: {sys.argv[1]}")
 
 
 if __name__ == "__main__":
